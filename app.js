dfab1804663f124f31e110d84453dcf1e8cee09f
Jonathan Tucker <71109818+jontucklogic@users.noreply.github.com>
2021-05-27 05:06:23 -0500

    Data and Route Parameters
    Created data for web app.

--- app.js
+++ app.js
@@ -9,50 +9,22 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-  console.log("Hello");
-  const err = new Error('Oh noes!');
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+app.use( (req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
   next(err);
 });
 
-app.use( (req, res, next) => {
-  console.log("world");
-  next();
-})
-
-
-
-
-app.get("/", (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.render('index', { name });
-  } else {
-    res.redirect('/hello');
-  }  
-});
-
-app.get("/cards", (req, res) => {
-    res.render('card', { prompt: "Who is buried in Grant's tomb?", colors });
-  });
-
-app.get('/hello', (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.redirect('/');
-  } else {
-    res.render('hello');
-  }
-});
-
-app.post('/hello', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/hello');
+app.use( (err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
 });
 
 app.listen(3000, () => {
